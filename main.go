82560d4232899634aaa392f9394cbf7d72075a07
Justin Chu <justin@VivoBook-ASUSLaptop-E410MAB-L410MA-b40826f4.localdomain>
2023-12-12 18:05:51 -0500

    working connection to openai api

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
+	// "bytes"
+	// "encoding/json"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
+	// "io"
+	// "log"
+	// "net/http"
+	"context"
 	"os"
 
+	openai "github.com/sashabaranov/go-openai"
 	"github.com/spf13/cobra"
 )
-
-
 
 func main(){
 	var rootCmd = &cobra.Command {
@@ -25,37 +25,26 @@ func main(){
 		Short: "Generate documentation for a given source file",
 		Args: cobra.ExactArgs(1),
 		Run:func(cmd *cobra.Command, args []string) {
-			// filename, err := os.ReadFile(args[0])
-			// if err != nil {
-			// log.Printf("Failed to read file: %v", err)
-			// }
-		instructions := "Please generate a short 5 sentance paragraph about dinorsaurs"
-		requestBody, err := json.Marshal(map[string]string{
-				"prompt": instructions,
-			})
-			if err != nil {
-			log.Fatalf("Failed to read json Marshil: %v", err)
-			}
-			api_key := os.Getenv("API_KEY")
-			req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(requestBody))
-			if err != nil{
-			log.Fatalf("Failed to create requestt: %v", err)
-			}
-			req.Header.Set("Content-Type", "application/json")
-			req.Header.Set("Authorization", "Bearer "+api_key)
-			resp, err := http.DefaultClient.Do(req)
-			if err != nil{
-				log.Fatalf("Failed to send request: %v", err)
-				}
-				defer resp.Body.Close()
-				if resp.StatusCode != http.StatusOK {
-					log.Fatalf("Connection failed somewhere idk dont sue me")
-				}
-				paragraph, err := io.ReadAll(resp.Body)
-				if err != nil {
-					log.Fatal(err)
-				}
-			  log.Printf("Response: %v", paragraph)
+		client := openai.NewClient("")
+		resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Please write a 5 sentace paragraph about ducks.",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
 			},
 		}
 // https://api.openai.com/v1/chat/completions
